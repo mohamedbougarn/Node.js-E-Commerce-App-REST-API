db3c968e87f79807280299d19311b1b7cc0e48b3
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-05 19:36:32 +0100

    add auth routes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const mongoose = require("mongoose");
-//var mongoUrl = '"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"'
- 
+const userrouter = require("./routes/user"); 
+const authrouter = require("./routes/authentification"); 
+
+
+app.use(express.json());
 dotenv.config();
 
 
@@ -11,7 +14,13 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('Connected to MongoDB: %s'))
 .catch((err)=>{console.log('MongoDB connection error: %s \n', err)});
 
+app.get('/api/test/', ()=>{
+console.log('test request sesessful');
+})
 
+
+app.use("/api/auth",authrouter);
+app.use("/api/user",userrouter);
 
 
 app.listen(process.env.PORT || 3000 , ()=> {
