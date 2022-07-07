4ae2c5263b1ce7f5596a3c3b3291d19d68431e18
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-07 23:54:35 +0100

    cart model

---
+++ models/Cart.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const CartSchema = mongoose.schema({
+
+    userId: {type: 'string', required: true,unique: true},
+    products: [
+        {
+            productId: {
+                type: String,
+            },
+            quantity: {
+                type: 'number',
+                default: 1,
+                required: true
+            }, 
+        }
+    ]
+    },
+    {timestamps : true}
+
+);
+
+module.exports = mongoose.model('Cart', CartSchema);
