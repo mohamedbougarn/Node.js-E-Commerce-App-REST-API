54d872452ef6c1399f1c0e5ecbe15741fd437018
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-07 23:55:23 +0100

    product model

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+
+
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.schema({
+
+    title: {type: 'string', required: true,unique: true},
+    desc: {type: 'string', required: true,unique: true},
+    img: {type: 'string', required: true},
+    category: {type: 'array'},
+    size: {type: 'string'},
+    color: {type: 'string'},
+    price: {type: 'string', required: true},
+    },
+    {timestamps : true}
+
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
