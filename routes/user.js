41f19bcbecd3ff2777c9ba97a51d518596b6a848
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-16 18:32:54 +0100

    get user stats

--- routes/user.js
+++ routes/user.js
@@ -73,13 +73,16 @@ router.delete('/:id',veriftokenauthorisation,async(req,res)=>{
 /**
  * GET all user
  */
- router.get('/find/:id',veriftokenAdmin,async(req,res)=>{
+ router.get('/',veriftokenAdmin,async(req,res)=>{
 
+    const query = req.query.new
   try {//for delete user info
 
-    const user = await User.findById(req.params.id)
-    const {password,...others} = user._doc;
-    res.status(200).json(others)
+    const users = query 
+    ? await User.find().sort({_id : -1}).limit(1) 
+    : await User.find()
+
+    res.status(200).json(users)
   }
   catch(err)
   {
@@ -87,6 +90,41 @@ router.delete('/:id',veriftokenauthorisation,async(req,res)=>{
   }
 
 })
+
+
+
+
+/**
+ * GET user stats for statistics user register per month
+ */
+router.get('/stats',veriftokenAdmin,async(req,res)=>{
+  const date = new Date();
+  const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      {$match: {createdAt : {$gte : lastyear} }},
+      {
+        $project: {
+          month : { $month: "$createdAt"},
+        },
+      },
+      {
+        $group: { 
+          _id : "$month",
+          total : {$sum : 1},
+        }
+      }
+    ]);
+    res.status(200).json(data);
+  }
+  catch(err){
+    res.status(500).json(err)
+  }
+
+
+
+});
+
 
 
 module.exports = router;
