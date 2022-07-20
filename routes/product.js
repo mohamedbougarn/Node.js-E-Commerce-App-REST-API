f5d151e5797968093318365f1c079b160b9a3569
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-20 16:59:58 +0100

    create and update methods in routes

--- routes/product.js
+++ routes/product.js
@@ -25,13 +25,13 @@ router.post('/',veriftokenAdmin,async(req,res)=>
 
 
 /***
- * UPDATE user
+ * UPDATE product fid by id
  */
 router.put('/:id', veriftokenAdmin, async(req, res)=>{
 
 
   try {//for update product by id info
-    const updateprod = await User.findByIdAndUpdate(
+    const updateprod = await Product.findByIdAndUpdate(
         req.params.id,
         {
         $set: req.body,
@@ -48,98 +48,76 @@ router.put('/:id', veriftokenAdmin, async(req, res)=>{
 })
 
 
-// /**
-//  * DELATE user
-//  */
-// router.delete('/:id',veriftokenauthorisation,async(req,res)=>{
+/**
+ * DELATE product
+ */
+router.delete('/:id',veriftokenAdmin,async(req,res)=>{
 
-//   try {//for delete user info
-//   await User.findByIdAndDelete(req.params.id)
-//   res.status(200).json("User deleted seccessfully !")
-//   }
-//   catch(err)
-//   {
-//     res.status(500).json(err)
-//   }
+  try {//for delete user info
+  await product.findByIdAndDelete(req.params.id)
+  res.status(200).json("product deleted seccessfully !")
+  }
+  catch(err)
+  {
+    res.status(500).json(err)
+  }
 
-// })
-
-
-
-// /**
-//  * GET user
-//  */
-//  router.get('/find/:id',veriftokenAdmin,async(req,res)=>{
-
-//   try {//for delete user info
-
-//     const user = await User.findById(req.params.id)
-//     const {password,...others} = user._doc;
-//     res.status(200).json(others)
-//   }
-//   catch(err)
-//   {
-//     res.status(500).json(err)
-//   }
-
-// })
-
-
-// /**
-//  * GET all user
-//  */
-//  router.get('/',veriftokenAdmin,async(req,res)=>{
-
-//     const query = req.query.new
-//   try {//for delete user info
-
-//     const users = query 
-//     ? await User.find().sort({_id : -1}).limit(1) 
-//     : await User.find()
-
-//     res.status(200).json(users)
-//   }
-//   catch(err)
-//   {
-//     res.status(500).json(err)
-//   }
-
-// })
+})
 
 
 
+/**
+ * GET product info
+ */
+ router.get('/find/:id',veriftokenAdmin,async(req,res)=>{
 
-// /**
-//  * GET user stats for statistics user register per month
-//  */
-// router.get('/stats',veriftokenAdmin,async(req,res)=>{
-//   const date = new Date();
-//   const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
-//   try {
-//     const data = await User.aggregate([
-//       {$match: {createdAt : {$gte : lastyear} }},
-//       {
-//         $project: {
-//           month : { $month: "$createdAt"},
-//         },
-//       },
-//       {
-//         $group: { 
-//           _id : "$month",
-//           total : {$sum : 1},
-//         }
-//       }
-//     ]);
-//     res.status(200).json(data);
-//   }
-//   catch(err){
-//     res.status(500).json(err)
-//   }
+  try {//for 
+
+    const product = await Product.findById(req.params.id)
+    res.status(200).json(product)
+  }
+  catch(err)
+  {
+    res.status(500).json(err)
+  }
+
+})
 
 
+/**
+ * GET all products info
+ */
+ router.get('/',async(req,res)=>{
 
-// });
+    const querynew = req.query.new
+    const qcategory = req.query.category
+  try {//for delete user info
 
+    let products;
+    if(querynew)
+    {
+        products = await Product.find().sort({createdAt : -1}).limit(5);
+    }
+    else if(qcategory)
+    {   
+        products = await Product.find({category : {
+            $in : [qcategory],
+        }, 
+
+    })
+    }else
+    {
+        products = await Product.find();
+
+    }
+    res.status(200).json(products)
+  }
+  catch(err)
+  {
+    res.status(500).json(err)
+  }
+
+})
 
 
 module.exports = router;
