a85d5e893a837380385a4f1124da2ece012592f8
mohamedbougarn <mohamedbougarn@gmail.com>
2022-08-17 18:47:28 +0100

    update order controller

---
+++ controllers/order.js
@@ -0,0 +1,67 @@
+const {veriftoken,veriftokenauthorisation, veriftokenAdmin} = require("../controllers/verifyToken")
+//const router = require('express').Router();
+const Order = require("../models/Order");
+
+
+/**
+ * @create order 
+ */
+//router.post('/',veriftoken,async(req,res)=>
+const CreateOrder = async(req,res)=>
+{
+    eriftoken = req.headers.authorization;
+    const newOrder = new Order(req.body)
+    try 
+    {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    }
+    catch(err) 
+    {
+        res.status(500).json(err)
+    }
+
+};
+
+
+
+/***
+ * UPDATE order fid by id
+ */
+//router.put('/:id', veriftokenAdmin, async(req, res)=>{
+constUpdatedOrderId =  async(req, res)=>{
+    veriftokenAdmin = req.headers.authorization;
+  try {//for update product by id info
+    const updateOrder = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+        $set: req.body,
+    },
+    {
+        new : true
+    });
+    res.status(200).json(updateOrder)
+  }
+  catch(e) {
+      res.status(500).json(e)
+  }
+
+};
+
+
+/**
+ * DELATE order
+ */
+//router.delete('/:id',veriftokenAdmin,async(req,res)=>{  
+const DelateOrderById = async(req,res)=>{
+veriftokenAdmin = req.headers.authorization;
+  try {//for delete user info
+  await Order.findByIdAndDelete(req.params.id)
+  res.status(200).json("product deleted seccessfully !")
+  }
+  catch(err)
+  {
+    res.status(500).json(err)
+  }
+
+}
