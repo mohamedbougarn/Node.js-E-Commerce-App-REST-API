f95f4d86de1000313eb81e44e02f01994afb327b
mohamedbougarn <mohamedbougarn@gmail.com>
2022-08-19 20:08:07 +0100

    update order controller

--- controllers/order.js
+++ controllers/order.js
@@ -9,7 +9,7 @@ const Order = require("../models/Order");
 //router.post('/',veriftoken,async(req,res)=>
 const createOrder = async(req,res)=>
 {
-    veriftoken = req.headers.authorization;
+    //veriftoken = req.headers.authorization;
     const newOrder = new Order(req.body)
     try 
     {
@@ -30,7 +30,7 @@ const createOrder = async(req,res)=>
  */
 //router.put('/:id', veriftokenAdmin, async(req, res)=>{
 const updatedOrderId =  async(req, res)=>{
-    veriftokenAdmin = req.headers.authorization;
+    //veriftokenAdmin = req.headers.authorization;
   try {//for update product by id info
     const updateOrder = await Order.findByIdAndUpdate(
         req.params.id,
@@ -54,7 +54,7 @@ const updatedOrderId =  async(req, res)=>{
  */
 //router.delete('/:id',veriftokenAdmin,async(req,res)=>{  
 const delateOrderById = async(req,res)=>{
-veriftokenAdmin = req.headers.authorization;
+//veriftokenAdmin = req.headers.authorization;
   try {//for delete user info
   await Order.findByIdAndDelete(req.params.id)
   res.status(200).json("product deleted seccessfully !")
@@ -71,7 +71,7 @@ veriftokenAdmin = req.headers.authorization;
  * get('/find/:userId'
  */
  const getUserOrderProductInfo = async(req,res)=>{
-    veriftokenauthorisation = req.headers.authorization;
+   // veriftokenauthorisation = req.headers.authorization;
     try {//for 
   
       const orders = await Orders.find({userID: req.params.userId});
@@ -90,7 +90,7 @@ veriftokenAdmin = req.headers.authorization;
    * get('/')
    */
    const getAll =async(req,res)=>{
-    veriftokenAdmin= req.headers.authorization;
+    //veriftokenAdmin= req.headers.authorization;
     try {
       let orders;
           orders = await Orders.find();
@@ -113,7 +113,7 @@ veriftokenAdmin = req.headers.authorization;
   
   const getMonthlyIncoming = async(req, res)=>{
 
-    veriftokenAdmin = req.headers.authorization;
+    ///veriftokenAdmin = req.headers.authorization;
     const date = new Date();
     const lastmonth = new Date(date.setMonth(date.getMonth() -1 ));
     const pmonth = new Date(new Date().setMonth(lastmonth.getMonth() -1 ));
@@ -145,14 +145,17 @@ veriftokenAdmin = req.headers.authorization;
   
   
   
-  module.exports = {
+  module.exports = 
+  {
+
     createOrder,
     getAll,
     getUserOrderProductInfo,
     delateOrderById,
     getMonthlyIncoming,
     updatedOrderId
-};
+    
+  };
   
   
   
