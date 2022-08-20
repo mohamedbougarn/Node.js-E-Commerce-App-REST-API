a6fde2060888de6cf5840e085a7a1de226f0024c
mohamedbougarn <mohamedbougarn@gmail.com>
2022-08-20 10:22:08 +0100

    stripe route for pay method

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,27 +1,9 @@
 const router = require('express').Router();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripecontroller = require('../controllers/stripe');
 
-router.post('/pay',(req, res)=>{
-    stripe.charges.create({
-        source:req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd",
-
-    },(stripeErr,stripeSucc)=>{
-        if (stripeErr) 
-        {
-            res.status(500).json({error: stripeerr});    
-        } else 
-        {
-            res.status(200).json({success: stripeSucc});
-    
-        }
-    });
-
-
-
-});
+router.post('/pay',stripecontroller.pay);
 
 
 module.exports = router
